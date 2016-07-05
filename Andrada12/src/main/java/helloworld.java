4553eed3f6c1bb2c5ce0d31c10d44830438489c9
Andrada <Andrada@Serban>
2016-07-05 11:24:21 +0300

    my_first_jsp

--- Andrada12/src/main/java/helloworld.java
+++ Andrada12/src/main/java/helloworld.java
@@ -1,13 +1,10 @@
 
-
 /**
  * Hello world!
  *
  */
-public class helloworld
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+public class helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello World!");
+	}
 }
